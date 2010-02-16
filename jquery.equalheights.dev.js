51fb320dc37ba0ed91e4e504c528e6bba0e43ceb
Ollie Relph <ollie@relph.me>
2010-02-16 11:36:18 +0000

    initial commit

---
+++ jquery.equalheights.dev.js
@@ -0,0 +1,29 @@
+/*
+ * jQuery EqualHeights Plugin
+ * version: 0.1
+ * URL: http://github.com/BBB/jquery-equalheights
+ * Description: Will add the height of the tallest child to all children.
+ * Requires: 1.3.2+ (Not tested on anything lower, but it'll probably work).
+ * Author: Oliver Relph
+ * Copyright: Copyright 2010 Oliver Relph
+ */
+
+(function($) {
+	$.fn.equalheights = function(children) {
+		
+		return this.each(function() {
+
+			var $this = $(this);
+			var $children = $(children, $this);
+			
+		  	var highest = 0;
+			$children.each(function(){
+				var h = $(this).height();
+				if (h > highest) {
+					highest = h
+				}
+			}).height(highest);
+
+		});
+	};
+})(jQuery);
